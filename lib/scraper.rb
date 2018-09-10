4034af5a59abe94d2cdd810ac1c6f8a93fb4db6d
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-10 16:28:00 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,17 +18,15 @@ class Scraper
   
   def get_page
     html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
-    @doc = Nokogiri::HTML(html)
-    @doc
+    doc = Nokogiri::HTML(html)
   end 
   
   def get_courses
-    @courses = get_page.css("#course-grid .posts-holder .post")
-    @courses
+    self.get_page.css("#course-grid .posts-holder .post")
   end
   
   def make_courses
-    @courses.each do |course| 
+    self.get_courses.each do |i| 
       course = Course.new
       course.title = course.css("h2").text
       course.schedule = course.css("em").text
@@ -37,6 +35,25 @@ class Scraper
   end 
   
 end
+
+
+
+def get_page
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+
+  def get_courses
+    self.get_page.css(".post")
+  end
+
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
 
 
 
