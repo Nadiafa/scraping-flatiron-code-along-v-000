8528089fecee05fbf2f32f2f0bcedb82448f436f
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-10 15:58:23 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,14 +1,15 @@
-require_relative './scraper.rb'
+# require_relative './scraper.rb'
+
 class Course 
-  attr_accessor :title, :schedule, :description
-  @@all = []
+  # attr_accessor :title, :schedule, :description
+  # @@all = []
   
-  def initialize
-    @@all << self 
-  end
+  # def initialize
+  #   @@all << self 
+  # end
   
-  def self.all 
-    @@all 
-  end 
+  # def self.all 
+  #   @@all 
+  # end 
   
 end 
